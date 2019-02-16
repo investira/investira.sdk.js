a408d2d9c9b49bb5aac169db543f522e8f8719f0
Ricardo Villar <ricardo.villar@dbsoft.com.br>
2019-02-16 08:41:14 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+exports.dates = require("./lib/utils/dates");
+exports.objects = require("./lib/utils/tokens");
+exports.passwords = require("./lib/utils/passwords");
+exports.sqls = require("./lib/utils/tokens");
+exports.tokens = require("./lib/utils/tokens");
+exports.validators = require("./lib/utils/validators");
+exports.numbers = require("./lib/utils/numbers");
+exports.numbers = require("./lib/utils/numbers");
+
+exports.crud = require("./lib/hofs/crud");
+exports.dao = require("./lib/hofs/dao");
+// exports.dao2 = require("./lib/hofs/dao2");
+exports.model = require("./lib/hofs/model");
+exports.mySqlServer = require("./lib/dbs/mySqlServer");
+
+exports.httpCors = require("./lib/middlewares/httpCors");
+exports.hostsCheckPoint = require("./lib/middlewares/hostsCheckPoint");
+
+exports.requestContext = require("./lib/middlewares/requestContext").requestContext;
+exports.requestContextEvents = require("./lib/middlewares/requestContext").requestContextEvents;
+exports.requestContextMiddleware = require("./lib/middlewares/requestContext").requestContextMiddleware;
+
+//GLobals-------------------------
+//Log
+gLog = require("./lib/log");
