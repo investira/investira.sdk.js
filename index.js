029e427fc4168906ad8c787e34f3e1d154a1d26f
Ricardo Villar <ricardo.villar@dbsoft.com.br>
2019-02-19 19:55:49 -0300

    Novo módulo strings

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-module.exports.dates = require("./lib/utils/dates");
-module.exports.formats = require("./lib/utils/formats");
-module.exports.httpRequests = require("./lib/utils/httpRequests");
-module.exports.invests = require("./lib/utils/invests");
-module.exports.numbers = require("./lib/utils/numbers");
-module.exports.objects = require("./lib/utils/objects");
-module.exports.validators = require("./lib/utils/validators");
+module.exports.dates = require('./lib/utils/dates');
+module.exports.formats = require('./lib/utils/formats');
+module.exports.httpRequests = require('./lib/utils/httpRequests');
+module.exports.invests = require('./lib/utils/invests');
+module.exports.numbers = require('./lib/utils/numbers');
+module.exports.objects = require('./lib/utils/objects');
+module.exports.strings = require('./lib/utils/strings');
+module.exports.validators = require('./lib/utils/validators');
 
-module.exports.model = require("./lib/hofs/model");
+module.exports.dataModel = require('./lib/hofs/dataModel');
